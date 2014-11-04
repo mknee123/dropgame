ad68813d91e64388e5ca49e90b85eca0b2cb2f5c
mknee123 <mknee123@gmail.com>
2014-11-04 10:09:18 -0500

    Demo 2.2
    An in class demo showing how to create and destroy animated rain drops
    using code.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+//make an array to keep track of all existing drops
+var drop_array = new Array();
+
 /**
  * The Drop class is a blueprint for each raindrop we generate
  * @author  John Doe
@@ -17,7 +20,7 @@ function Drop(){
 		this.item_on_page.className = "raindrop";
 		//store a random x and y position, different for each drop. I'm using screen width of 800, height of 600:
 		this.x = Math.floor(Math.random()*800);
-		this.y = Math.floor(Math.random()*600);
+		this.y = -50;
 		//use those x and y coordinates in the CSS to position the drop:
 		this.item_on_page.style.left = this.x + "px";
 		this.item_on_page.style.top = this.y + "px";
@@ -28,18 +31,62 @@ function Drop(){
 	*   The destroy function does lots of cleaning up when a drop is removed from the page
 	*/
 	this.destroy = function(){
-		
+		//clear all splashes
+		for(var j=0; j<document.getElementsByClassName("splash").length; j++){
+			document.body.removeChild(document.getElementsByClassName("splash")[j]);	
+		}
+		//generate a copy of our animated GIF as an <img> tag:
+		var newsplash = document.createElement("img");
+		newsplash.className = "splash";
+		//make each path/filename unquie so it replays the animation
+		newsplash.src = "img/splash-anim-gif.gif?"+Math.random();
+		//style it and set its position to that of the drop
+		newsplash.style.position ="absolute";
+		newsplash.style.left = this.x+"px";
+		newsplash.style.top = this.y +"px";
+		//place it onto the HTML page
+		document.body.appendChild(newsplash);
+		//remove the drop object from the array so other functions stop trying to manipulate it:
+		var this_drops_index_num = drop_array.indexOf(this);
+		//splice a single item out of the array
+		drop_array.splice(this_drops_index_num, 1);
+		//remove the graphic of the drop from HTML page
+		document.body.removeChild(this.item_on_page);
 	}
 } //close the Drop class
 
-
+/**
+* This function moves all existing drops downward a little
+*
+*/
+function moveAllDrops(){
+	//console.log("moveAllDrops");
+	//loop through all the existing drops
+	for(var i=0; i<drop_array.length; i++){
+		//add to the y property of the drop
+	drop_array[i].y +=5;
+	drop_array[i].item_on_page.style.top = drop_array[i].y + 'px';
+	//if drop is touching floor
+	if(drop_array[i].y>400){
+		drop_array[i].destroy();
+	}
+	}
+}
+/**
+* This function creates a drop every so often
+*
+*/
+function spawn(){
+	//make an object that's an instance of the Drop Class:
+	var another_drop = new Drop();
+	another_drop.create();
+	drop_array.push(another_drop);
+}
 onload=init;
 
 function init() {
-	//make an object that's an instance of the Drop Class:
-	var drop1 = new Drop();
-	drop1.create();
-	//make an object that's an instance of the Drop Class:
-	var drop2 = new Drop();
-	drop2.create();
+	//make an drop bject every so often:
+	setInterval(function(){spawn()}, 500);
+	//start moving the drops a little, every so often:
+	setInterval(moveAllDrops, 1000/100);
 }
